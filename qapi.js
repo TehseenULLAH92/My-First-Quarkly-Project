0d08d93c42c500a375c4c206f9781a504168a605
Tehseen.K <tehseenullah92@gmail.com>
2020-10-20 18:45:32 +0500

    Some changes

---
+++ qapi.js
@@ -0,0 +1,49 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5f8c1c483d84d4001ec3b169",
+				"5f8c1c483d84d4001ec3b167",
+				"5f8ea814d9c9a8001f19f31b",
+				"5f8ea82241c25f001ec405bf",
+				"5f8ea82b3d84d4001ec3c537",
+				"5f8ea8543d84d4001ec3c53f"
+			]
+		},
+		"5f8c1c483d84d4001ec3b167": {
+			"id": "5f8c1c483d84d4001ec3b167",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5f8c1c483d84d4001ec3b169": {
+			"id": "5f8c1c483d84d4001ec3b169",
+			"name": "index",
+			"pageUrl": "index"
+		},
+		"5f8ea814d9c9a8001f19f31b": {
+			"id": "5f8ea814d9c9a8001f19f31b",
+			"pageUrl": "services",
+			"name": "Services"
+		},
+		"5f8ea82241c25f001ec405bf": {
+			"id": "5f8ea82241c25f001ec405bf",
+			"pageUrl": "projects",
+			"name": "Projects"
+		},
+		"5f8ea82b3d84d4001ec3c537": {
+			"id": "5f8ea82b3d84d4001ec3c537",
+			"pageUrl": "blog",
+			"name": "Blog"
+		},
+		"5f8ea8543d84d4001ec3c53f": {
+			"id": "5f8ea8543d84d4001ec3c53f",
+			"pageUrl": "contact-us",
+			"name": "Contact Us"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby"
+}
---
+++ src/pages/index.js
@@ -0,0 +1,102 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Strong, Text, Box, Image } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { Override, Menu, Section } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section quarkly-title="Top Banner" background="#000000">
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+				quarkly-title="Top Navbar"
+			>
+				<Box quarkly-title="Left Side Block">
+					<Text
+						margin="0"
+						md-margin="0px 0 20px 0"
+						text-align="left"
+						font="--lead"
+						quarkly-title="Logo Text"
+					>
+						<Strong>
+							<Link
+								href="#"
+								text-transform="uppercase"
+								text-decoration-line="initial"
+								font="700 28px/30px --fontFamily-sans"
+								letter-spacing="2px"
+								color="#ffffff"
+							>
+								Integrio
+							</Link>
+						</Strong>
+					</Text>
+				</Box>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+					quarkly-title="Center Menu Block"
+				>
+					<Override
+						slot="link"
+						text-decoration="none"
+						color="--light"
+						padding="6px 12px"
+						font="700 16px/24px --fontFamily-sans"
+						text-transform="uppercase"
+					/>
+					<Override
+						slot="link-active"
+						color="--light"
+						text-decoration-line="underline"
+						font="700 16px/24px --fontFamily-sans"
+						text-transform="uppercase"
+					/>
+					<Override slot="item" padding="6px" display="inline-block" color="#ffffff" />
+					<Override slot="link-index">
+						Home
+					</Override>
+					<Override slot="link-404">
+						Company
+					</Override>
+				</Menu>
+				<Box quarkly-title="Right Side Block" margin="0px 0px 0px 0px" padding="3px 10px 3px 10px">
+					<Image
+						width="25px"
+						height="25px"
+						min-width="25px"
+						min-height="25px"
+						src="https://uploads.quarkly.io/5f8c1c483d84d4001ec3b164/images/shopping-cart1-p-130x130q80.png?v=2020-10-20T09:46:07.646Z"
+						quarkly-title="Search"
+						margin="0 10px 0"
+					/>
+					<Image
+						width="25px"
+						height="25px"
+						min-width="25px"
+						min-height="25px"
+						src="https://uploads.quarkly.io/5f8c1c483d84d4001ec3b164/images/search-3-xxl.png?v=2020-10-20T13:41:34.941Z"
+						quarkly-title="Cart"
+						margin="0px 10px 0px"
+					/>
+				</Box>
+			</Box>
+		</Section>
+	</Theme>;
+});
